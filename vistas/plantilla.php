a3365decac3721d279f6a7e85309f45d13239078
ElianMartinez <elianmartinez157@gmail.com>
2021-08-16 20:32:41 -0400

    Agregando configuracion del servidor en un file externo

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -1,5 +1,5 @@
 <?php
-
+include "core/config.php";
 session_start();
 
 ?>
@@ -15,28 +15,28 @@ session_start();
 
   <?php
 
-  $favicon = new InicioC();
-  $favicon -> FaviconC();
+$favicon = new InicioC();
+$favicon->FaviconC();
 
-  ?>
+?>
 
-  
+
 
   <!-- Bootstrap 3.3.7 -->
-  <link rel="stylesheet" href="http://localhost/clinica/Vistas/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo $_SERVER ?>clinica/Vistas/bower_components/bootstrap/dist/css/bootstrap.min.css">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="http://localhost/clinica/Vistas/bower_components/font-awesome/css/font-awesome.min.css">
+  <link rel="stylesheet" href="<?php echo $_SERVER ?>clinica/Vistas/bower_components/font-awesome/css/font-awesome.min.css">
   <!-- Ionicons -->
-  <link rel="stylesheet" href="http://localhost/clinica/Vistas/bower_components/Ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="<?php echo $_SERVER ?>clinica/Vistas/bower_components/Ionicons/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="http://localhost/clinica/Vistas/dist/css/AdminLTE.min.css">
+  <link rel="stylesheet" href="<?php echo $_SERVER ?>clinica/Vistas/dist/css/AdminLTE.min.css">
   <!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  <link rel="stylesheet" href="http://localhost/clinica/Vistas/dist/css/skins/_all-skins.min.css">
+  <link rel="stylesheet" href="<?php echo $_SERVER ?>clinica/Vistas/dist/css/skins/_all-skins.min.css">
 
   <!-- DataTables -->
-  <link rel="stylesheet" href="http://localhost/clinica/Vistas/bower_components/datatables.net-bs/css/responsive.bootstrap.min.css">
-  <link rel="stylesheet" href="http://localhost/clinica/Vistas/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo $_SERVER ?>clinica/Vistas/bower_components/datatables.net-bs/css/responsive.bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo $_SERVER ?>clinica/Vistas/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
 
   <!-- FullCalendar -->
   <link rel="stylesheet" href="//localhost/clinica/Vistas/bower_components/fullcalendar/dist/fullcalendar.min.css">
@@ -59,125 +59,121 @@ session_start();
 
   <?php
 
-  if(isset($_SESSION["Ingresar"]) && $_SESSION["Ingresar"] == true){
+if (isset($_SESSION["Ingresar"]) && $_SESSION["Ingresar"] == true) {
 
     echo '<div class="wrapper">';
 
     include "modulos/cabecera.php";
 
-    if($_SESSION["rol"] == "Secretaria"){
+    if ($_SESSION["rol"] == "Secretaria") {
 
-      include "modulos/menuSecretaria.php";
+        include "modulos/menuSecretaria.php";
 
-    }else if($_SESSION["rol"] == "Paciente"){
+    } else if ($_SESSION["rol"] == "Paciente") {
 
-      include "modulos/menuPaciente.php";
+        include "modulos/menuPaciente.php";
 
-    }else if($_SESSION["rol"] == "Doctor"){
+    } else if ($_SESSION["rol"] == "Doctor") {
 
-      include "modulos/menuDoctor.php";
+        include "modulos/menuDoctor.php";
 
-    }else if($_SESSION["rol"] == "Administrador"){
+    } else if ($_SESSION["rol"] == "Administrador") {
 
-      include "modulos/menuAdmin.php";
+        include "modulos/menuAdmin.php";
 
     }
 
-    
-
     $url = array();
 
-    if(isset($_GET["url"])){
+    if (isset($_GET["url"])) {
 
-      $url = explode("/", $_GET["url"]);
+        $url = explode("/", $_GET["url"]);
 
-      if($url[0] == "inicio" || $url[0] == "salir" || $url[0] == "perfil-Secretaria" || $url[0] == "perfil-S" || $url[0] == "consultorios" || $url[0] == "E-C" || $url[0] == "doctores" || $url[0] == "pacientes" || $url[0] == "perfil-Paciente" || $url[0] == "perfil-P" || $url[0] == "Ver-consultorios" || $url[0] == "Doctor" || $url[0] == "historial" || $url[0] == "perfil-Doctor" || $url[0] == "perfil-D" || $url[0] == "Citas" || $url[0] == "perfil-Administrador" || $url[0] == "perfil-A" || $url[0] == "secretarias" || $url[0] == "inicio-editar" || $url[0] == "Csecretaria" || $url[0] == "Citasecretaria" || $url[0] == "historialSecretaria"){
+        if ($url[0] == "inicio" || $url[0] == "salir" || $url[0] == "perfil-Secretaria" || $url[0] == "perfil-S" || $url[0] == "consultorios" || $url[0] == "E-C" || $url[0] == "doctores" || $url[0] == "pacientes" || $url[0] == "perfil-Paciente" || $url[0] == "perfil-P" || $url[0] == "Ver-consultorios" || $url[0] == "Doctor" || $url[0] == "historial" || $url[0] == "perfil-Doctor" || $url[0] == "perfil-D" || $url[0] == "Citas" || $url[0] == "perfil-Administrador" || $url[0] == "perfil-A" || $url[0] == "secretarias" || $url[0] == "inicio-editar" || $url[0] == "Csecretaria" || $url[0] == "Citasecretaria" || $url[0] == "historialSecretaria") {
 
-        include "modulos/".$url[0].".php";
+            include "modulos/" . $url[0] . ".php";
 
-      }
+        }
 
-    }else{
+    } else {
 
         include "modulos/inicio.php";
 
     }
 
+    echo '</div>';
 
-      echo '</div>';
+} else if (isset($_GET["url"])) {
 
-    }else if(isset($_GET["url"])){
-
-      if($_GET["url"] == "seleccionar"){
+    if ($_GET["url"] == "seleccionar") {
 
         include "modulos/seleccionar.php";
 
-      }else if($_GET["url"] == "ingreso-Secretaria"){
+    } else if ($_GET["url"] == "ingreso-Secretaria") {
 
         include "modulos/ingreso-Secretaria.php";
 
-      }else if($_GET["url"] == "ingreso-Paciente"){
+    } else if ($_GET["url"] == "ingreso-Paciente") {
 
         include "modulos/ingreso-Paciente.php";
 
-      }else if($_GET["url"] == "ingreso-Doctor"){
+    } else if ($_GET["url"] == "ingreso-Doctor") {
 
         include "modulos/ingreso-Doctor.php";
 
-      }else if($_GET["url"] == "ingreso-Administrador"){
+    } else if ($_GET["url"] == "ingreso-Administrador") {
 
         include "modulos/ingreso-Administrador.php";
 
-      }
-
-    }else {
-
-      include "modulos/seleccionar.php";
-
     }
 
+} else {
 
-  ?>
+    include "modulos/seleccionar.php";
+
+}
+
+?>
 
   <!-- =============================================== -->
 
   <!-- Left side column. contains the sidebar -->
-  
+
 
   <!-- =============================================== -->
 
   <!-- Content Wrapper. Contains page content -->
- 
+
 
 <!-- ./wrapper -->
 
 <!-- jQuery 3 -->
-<script src="http://localhost/clinica/Vistas/bower_components/jquery/dist/jquery.min.js"></script>
+<script src="<?php echo $_SERVER ?>clinica/Vistas/bower_components/jquery/dist/jquery.min.js"></script>
 <!-- Bootstrap 3.3.7 -->
-<script src="http://localhost/clinica/Vistas/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="<?php echo $_SERVER ?>clinica/Vistas/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <!-- SlimScroll -->
-<script src="http://localhost/clinica/Vistas/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<script src="<?php echo $_SERVER ?>clinica/Vistas/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
 <!-- FastClick -->
-<script src="http://localhost/clinica/Vistas/bower_components/fastclick/lib/fastclick.js"></script>
+<script src="<?php echo $_SERVER ?>clinica/Vistas/bower_components/fastclick/lib/fastclick.js"></script>
 <!-- AdminLTE App -->
-<script src="http://localhost/clinica/Vistas/dist/js/adminlte.min.js"></script>
+<script src="<?php echo $_SERVER ?>clinica/Vistas/dist/js/adminlte.min.js"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="http://localhost/clinica/Vistas/dist/js/demo.js"></script>
+<script src="<?php echo $_SERVER ?>clinica/Vistas/dist/js/demo.js"></script>
 
 <!-- DataTables -->
-<script src="http://localhost/clinica/Vistas/bower_components/datatables.net/js/jquery.dataTables.js"></script>
-<script src="http://localhost/clinica/Vistas/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
-<script src="http://localhost/clinica/Vistas/bower_components/datatables.net-bs/js/dataTables.responsive.min.js"></script>
-<script src="http://localhost/clinica/Vistas/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<script src="<?php echo $_SERVER ?>clinica/Vistas/bower_components/datatables.net/js/jquery.dataTables.js"></script>
+<script src="<?php echo $_SERVER ?>clinica/Vistas/bower_components/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="<?php echo $_SERVER ?>clinica/Vistas/bower_components/datatables.net-bs/js/dataTables.responsive.min.js"></script>
+<script src="<?php echo $_SERVER ?>clinica/Vistas/bower_components/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
 
 <!-- fullCalendar -->
-<script src="http://localhost/clinica/Vistas/bower_components/moment/moment.js"></script>
-<script src="http://localhost/clinica/Vistas/bower_components/fullcalendar/dist/fullcalendar.min.js"></script>
-<script src="http://localhost/clinica/Vistas/bower_components/fullcalendar/dist/locale/es.js"></script>
+<script src="<?php echo $_SERVER ?>clinica/Vistas/bower_components/moment/moment.js"></script>
+<script src="<?php echo $_SERVER ?>clinica/Vistas/bower_components/fullcalendar/dist/fullcalendar.min.js"></script>
+<script src="<?php echo $_SERVER ?>clinica/Vistas/bower_components/fullcalendar/dist/locale/es.js"></script>
 
-<script src="http://localhost/clinica/Vistas/js/doctores.js"></script>
-<script src="http://localhost/clinica/Vistas/js/pacientes.js"></script>
-<script src="http://localhost/clinica/Vistas/js/secretarias.js"></script>
+<script src="<?php echo $_SERVER ?>clinica/Vistas/js/doctores.js"></script>
+<script src="<?php echo $_SERVER ?>clinica/Vistas/js/pacientes.js"></script>
+<script src="<?php echo $_SERVER ?>clinica/Vistas/js/secretarias.js"></script>
 
 
 
@@ -192,7 +188,7 @@ session_start();
         y    = date.getFullYear()
 
     $('#calendar').fullCalendar({
-      
+
      hiddenDays: [0, 6],
 
      defaultView: 'agendaWeek',
@@ -202,101 +198,89 @@ events:[
 
         <?php
 
-              $resultado = CitasC::VerCitasC();
+$resultado = CitasC::VerCitasC();
 
-        foreach ($resultado as $key => $value){
-          
-          if($value["id_doctor"] == substr($_GET["url"], 7)){
+foreach ($resultado as $key => $value) {
 
-            echo '{
+    if ($value["id_doctor"] == substr($_GET["url"], 7)) {
+
+        echo '{
+              id: ' . $value["id"] . ',
+              title: "' . $value["nyaP"] . '",
+              start: "' . $value["inicio"] . '",
+              end: "' . $value["fin"] . '"
+            },';
+
+    } else if ($value["id_doctor"] == substr($_GET["url"], 6)) {
+
+        echo '{
 
 
-              id: '.$value["id"].',
-              title: "'.$value["nyaP"].'",
-              start: "'.$value["inicio"].'",
-              end: "'.$value["fin"].'"
+              id: ' . $value["id"] . ',
+              title: "' . $value["nyaP"] . '",
+              start: "' . $value["inicio"] . '",
+              end: "' . $value["fin"] . '"
 
 
             },';
 
+    }if ($value["id_doctor"] == substr($_GET["url"], 15)) {
+
+        echo '{
 
 
-          }else if($value["id_doctor"] == substr($_GET["url"], 6)){
-
-             echo '{
-
-
-              id: '.$value["id"].',
-              title: "'.$value["nyaP"].'",
-              start: "'.$value["inicio"].'",
-              end: "'.$value["fin"].'"
-
-
-            },';
-
-
-          } if($value["id_doctor"] == substr($_GET["url"], 15)){
-
-            echo '{
-
-
-              id: '.$value["id"].',
-              title: "'.$value["nyaP"].'",
-              start: "'.$value["inicio"].'",
-              end: "'.$value["fin"].'"
+              id: ' . $value["id"] . ',
+              title: "' . $value["nyaP"] . '",
+              start: "' . $value["inicio"] . '",
+              end: "' . $value["fin"] . '"
 
 
             },';
-          }
-        }
+    }
+}
 
-        ?>
+?>
 
 
 ],
-   
+
    <?php
 
-      if($_SESSION["rol"] == "Paciente"){
+if ($_SESSION["rol"] == "Paciente") {
 
-        $columna = "id";
-        $valor = substr($_GET["url"], 7);
+    $columna = "id";
+    $valor = substr($_GET["url"], 7);
 
-        $resultado = DoctoresC::DoctorC($columna, $valor);
+    $resultado = DoctoresC::DoctorC($columna, $valor);
 
-        echo 'scrollTime: "'.$resultado["horarioE"].'",
-              minTime: "'.$resultado["horarioE"].'",
-              maxTime: "'.$resultado["horarioS"].'",';
+    echo 'scrollTime: "' . $resultado["horarioE"] . '",
+              minTime: "' . $resultado["horarioE"] . '",
+              maxTime: "' . $resultado["horarioS"] . '",';
 
+} else if ($_SESSION["rol"] == "Doctor") {
 
+    $columna = "id";
+    $valor = substr($_GET["url"], 6);
 
-      }else if($_SESSION["rol"] == "Doctor"){
+    $resultado = DoctoresC::DoctorC($columna, $valor);
 
-        $columna = "id";
-        $valor = substr($_GET["url"], 6);
+    echo 'scrollTime: "' . $resultado["horarioE"] . '",
+              minTime: "' . $resultado["horarioE"] . '",
+              maxTime: "' . $resultado["horarioS"] . '",';
 
-        $resultado= DoctoresC::DoctorC($columna, $valor);
+}if ($_SESSION["rol"] == "Secretaria") {
 
-        echo 'scrollTime: "'.$resultado["horarioE"].'",
-              minTime: "'.$resultado["horarioE"].'",
-              maxTime: "'.$resultado["horarioS"].'",';
+    $columna = "id";
+    $valor = substr($_GET["url"], 15);
 
+    $resultado = DoctoresC::DoctorC($columna, $valor);
 
+    echo 'scrollTime: "' . $resultado["horarioE"] . '",
+              minTime: "' . $resultado["horarioE"] . '",
+              maxTime: "' . $resultado["horarioS"] . '",';
+}
 
-      }if($_SESSION["rol"] == "Secretaria"){
-
-         $columna = "id";
-        $valor = substr($_GET["url"], 15);
-
-        $resultado= DoctoresC::DoctorC($columna, $valor);
-
-        echo 'scrollTime: "'.$resultado["horarioE"].'",
-              minTime: "'.$resultado["horarioE"].'",
-              maxTime: "'.$resultado["horarioS"].'",';
-      }
-
-
-   ?>
+?>
 
 dayClick:function(date,jsEvent,view){
 
@@ -331,10 +315,6 @@ dayClick:function(date,jsEvent,view){
       $('#fechaz').val(fecha[0]);
 
 }
-
-        })
-
-
 
 </script>
 
